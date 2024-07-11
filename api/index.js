18ff1e61dc97dc4d1e7534f77b43feb2c10fa535
Tushar Mundhra <tusharmundhra444@gmail.com>
2024-07-12 03:44:48 +0530

    make changes for uploading images on cloudinary

--- api/index.js
+++ api/index.js
@@ -26,22 +26,6 @@ app.use(
 );
 app.use(cookieParser());
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "https://zippy-salmiakki-2654a5.netlify.app/dist/upload/");
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + file.originalname);
-  },
-});
-
-const upload = multer({ storage: storage });
-
-app.post("/api/upload", upload.single("file"), (req, res) => {
-  const file = req.file;
-  res.status(200).json(file.filename);
-});
-
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
@@ -49,6 +33,7 @@ app.use("/api/comments", commentRoutes);
 app.use("/api/likes", likeRoutes);
 app.use("/api/relationships", relationshipsRoutes);
 app.use("/api/news", newsRoutes);
+app.use("/api/upload", uploadRoutes);
 
 app.listen(8800, () => {
   console.log("Connected to backend!");
